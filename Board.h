c9f7e62a99eca64b6e2e60166b3d4f5bacd5cb80
drumi <software.1251@gmail.com>
2020-07-22 05:32:40 +0300

    improved board

--- Board.h
+++ Board.h
@@ -1,13 +1,16 @@
 #pragma once
+
 #include "Piece.h"
 #include <vector>
 
 class Board
 {
     private:
-    std::vector<Piece*> pieces;
+    Piece m_board[8][8];
     void init();
 
     public:
     Board();
+    void move(int x, int y, int xdest, int ydest); //TODO push on stack history
+    Piece const** getPieces() const { return (Piece const**)m_board; }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Board.h"
 
 int main()
 {
-    
+    Board b;
+   
 }
