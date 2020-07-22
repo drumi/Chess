f390471519083a7a5d9a3bd056966222b9c6626e
drumi <software.1251@gmail.com>
2020-07-22 17:41:03 +0300

    bug fixes

--- Board.cpp
+++ Board.cpp
@@ -34,8 +34,8 @@ void Board::init()
     }
 
     m_board[0][0] = Piece(false, PieceType::ROOK);
-    m_board[0][1] = Piece(false, PieceType::BISHOP);
-    m_board[0][2] = Piece(false, PieceType::KNIGHT);
+    m_board[0][1] = Piece(false, PieceType::KNIGHT);
+    m_board[0][2] = Piece(false, PieceType::BISHOP);
     m_board[0][3] = Piece(false, PieceType::QUEEN);
     m_board[0][4] = Piece(false, PieceType::KING);
     m_board[0][5] = Piece(false, PieceType::BISHOP);
@@ -43,8 +43,8 @@ void Board::init()
     m_board[0][7] = Piece(false, PieceType::ROOK);
 
     m_board[7][0] = Piece(true, PieceType::ROOK);
-    m_board[7][1] = Piece(true, PieceType::BISHOP);
-    m_board[7][2] = Piece(true, PieceType::KNIGHT);
+    m_board[7][1] = Piece(true, PieceType::KNIGHT);
+    m_board[7][2] = Piece(true, PieceType::BISHOP);
     m_board[7][3] = Piece(true, PieceType::QUEEN);
     m_board[7][4] = Piece(true, PieceType::KING);
     m_board[7][5] = Piece(true, PieceType::BISHOP);
@@ -56,6 +56,6 @@ void Board::move(int x, int y, int xdest, int ydest)
 {
     assert(x < 8 && y < 8 && xdest < 8 && ydest < 8 && x >=0 && y >= 0 && xdest >= 0 && ydest >= 0);
 
-    m_board[x][y].setMoved();
-    m_board[xdest][ydest] = m_board[x][y];
+    m_board[y][x].setMoved();
+    m_board[ydest][xdest] = m_board[y][x];
 }
--- MoveValidator.cpp
+++ MoveValidator.cpp
@@ -1,45 +1,51 @@
 #include "MoveValidator.h"
 #include <cassert>
+#include <iostream>
 
 bool MoveValidator::isValid(Board const& board, int x, int y, int xdest, int ydest, int xEnpassant)
 {
     boardArrPtr pieces = board.getPieces();
 
+    // Check if coordinates are out of bounds or they are the same
     if(x > 8 || y > 8 || x < 0 || y < 0 || xdest > 8 || ydest > 8 || xdest < 0 || ydest < 0 || (x == xdest && y == ydest))
         return false;
+    // Check if we go on piece with same color
     if((*pieces)[y][x].isWhite() == (*pieces)[ydest][xdest].isWhite() && (*pieces)[ydest][xdest].getType() != PieceType::EMPTY)
         return false;    
 
     Piece piece = (*pieces)[y][x];
-
+    // Check if move is valid for piece (does not check illegal moves for exposing king)
     switch (piece.getType())
     {
         case PieceType::EMPTY:
             return false;
             break;
         case PieceType::PAWN:
-            if(ydest - y >= 0 && piece.isWhite() || ydest - y <= 0 && !piece.isWhite())
+            if(ydest - y >= 0 && piece.isWhite() || ydest - y <= 0 && !piece.isWhite()) // Check if pawn moves in the right direction
                 return false;
-            else if(x == xdest && (*pieces)[ydest][xdest].getType() != PieceType::EMPTY)
+            else if(x == xdest && (*pieces)[ydest][xdest].getType() != PieceType::EMPTY) // Check if when pawn moves forward there are no figures blocking
                 return false;
-            else if(abs(x - xdest) > 1 || abs(y - ydest) > 2 || (abs(y - ydest) > 1 && piece.hasMoved()))
+            else if(abs(x - xdest) > 1 || abs(y - ydest) > 2 || (abs(y - ydest) > 1 && piece.hasMoved())) // Check if pawn moves more than 2 (or 1 with first move) squares
                 return false;
             else if(abs(x - xdest) == 1)
             {
-                if((*pieces)[ydest][xdest].getType() == PieceType::EMPTY)
+                if(abs(y - ydest) == 2) // Check if we have tried taking piece on first move by moving 2 squares
+                    return false;
+                else if((*pieces)[ydest][xdest].getType() == PieceType::EMPTY)
                 {
-                    if(xdest != xEnpassant)
+                    if(xdest != xEnpassant) // Check en passant rule
                         return false;
                     else if(piece.isWhite() && ydest != 1 || !piece.isWhite() && ydest != 6)
                         return false;
-                }      
-                else if((*pieces)[ydest][xdest].isWhite() == piece.isWhite())
-                    return false;
+                } 
             }
             break;
         case PieceType::ROOK:
             break;
         case PieceType::KNIGHT:
+            // Check G move 
+            if(!(abs(x - xdest) == 1 && abs(y - ydest) == 2 || abs(x - xdest) == 2 && abs(y - ydest) == 1))
+                return false;
             break;
         case PieceType::BISHOP:
             break;
