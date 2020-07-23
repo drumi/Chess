11c81ff22fbf4d72f4eaeb6324c60463d619e318
drumi <software.1251@gmail.com>
2020-07-23 22:23:49 +0300

    made ai play random moves

--- ChessAI.cpp
+++ ChessAI.cpp
@@ -1,6 +1,11 @@
 #include "ChessAI.h"
 
+#include <stdlib.h>
+#include <time.h> 
+
 MoveResult ChessAI::GetMove(Board const& board, bool isWhiteTurn, int depth)
 {
-    return MoveGenerator::Generate(board, isWhiteTurn)[0];
+    srand (time(NULL));
+    std::vector<MoveResult> r = MoveGenerator::Generate(board, isWhiteTurn);
+    return r[rand() % r.size()];
 }
